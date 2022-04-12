1ed8fe4a5cd6fbe8266680e79938473cd72e6e58
Lucia <pongu80@gmail.com>
2022-04-11 19:08:22 -0500

    added function that renders API data

---
+++ js/main.js
@@ -0,0 +1,38 @@
+const newsBoxes = document.getElementById('news-boxes');
+
+// Grabs an array of news data from API
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Host': 'newsx.p.rapidapi.com',
+		'X-RapidAPI-Key': '8428b84edcmshf01bf1574f2b107p19470cjsndb8888e29baa'
+	}
+};
+
+fetch('https://newsx.p.rapidapi.com/search?limit=10&skip=0', options)
+	.then(response => response.json())
+	.then(response => mapNews(response))
+	.catch(err => console.error(err));
+
+// Map out each API news data and render it inside newsBox div
+function mapNews(resp) {
+    return resp.map(data => {
+        const newsBox = document.createElement('div');
+        newsBox.innerHTML = `
+            <img class="news-img" src="${data.image}" alt="${data.title}">
+            <h1 class="news-title"><a href="${data.url}">${data.title}</a></h1>
+            <p class="news-summary">${shortenContent(data.summary)}</p>
+        `
+        newsBoxes.appendChild(newsBox);
+    })
+}
+
+// summary will be shortened to ... after 15th word
+function shortenContent(content) {
+    if(content.length > 15) {
+        return content.split(' ').slice(0, 15).join(' ') + '...';
+    }
+    else {
+        return content;
+    }
+}
